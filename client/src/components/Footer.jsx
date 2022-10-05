572203bad24fba7b8900ec6416138d6f787c21f3
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2022-10-05 10:37:19 +0100

    Add Navbar and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Logo from '../img/logo.jpg';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<img src={Logo} alt="" />
+			<span>
+				Made with <b>ReactJS</b> by <i>Cenech Solutions</i>
+			</span>
+		</footer>
+	);
 };
 
 export default Footer;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../img/logo.jpg';
 
 const NavBar = () => {
 	return (
 		<div className="navbar">
 			<div className="container">
-				<div className="logo">Logo</div>
-				<div className="links">Links</div>
+				<div className="logo">
+					<img src={Logo} alt="" />
+				</div>
+				<div className="links">
+					<Link className="link" to="/?cat=art">
+						Art
+					</Link>
+					<Link className="link" to="/?cat=science">
+						Science
+					</Link>
+					<Link className="link" to="/?cat=technology">
+						Technology
+					</Link>
+					<Link className="link" to="/?cat=cinema">
+						Cinema
+					</Link>
+					<Link className="link" to="/?cat=culture">
+						Culture
+					</Link>
+					<Link className="link" to="/?cat=politics">
+						Politics
+					</Link>
+					<span>John</span>
+					<span>Log out</span>
+					<span className="write">
+						<Link to="/write">write</Link>
+					</span>
+				</div>
 			</div>
 		</div>
 	);
